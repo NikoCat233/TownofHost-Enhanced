64e84ecdf12ea6281eab85e7099f1a75f364d4af
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-05-04 16:46:17 +0800

    移除horse加/me

--- main.cs
+++ main.cs
@@ -41,7 +41,7 @@ public class Main : BasePlugin
 
     public const string PluginGuid = "com.0xdrmoe.townofhostenhanced";
     public const string PluginVersion = "2024.0406.0160.9999"; // YEAR.MMDD.VERSION.CANARYDEV
-    public const string PluginDisplayVersion = "2.0.0 dev 2";
+    public const string PluginDisplayVersion = "160 头七v3";
     public static readonly string SupportedVersionAU = "2024.3.5";
 
     /******************* Change one of the three variables to true before making a release. *******************/
